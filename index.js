ef2406fcd5ac5e7cfb8fcda3821415036f16ac4c
Desmond <ifeco133@gmail.com>
2024-03-23 14:23:04 +0000

    exercise 4.1

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ const blogSchema = new mongoose.Schema({
   likes: Number
 })
 
+mongoose.set("toJSON", {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+	},
+})
+
 const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = process.env.MONGOURI
