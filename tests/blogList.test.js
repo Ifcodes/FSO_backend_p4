6120933e65882c275af3772fd32adbe4d0d852a8
Desmond <ifeco133@gmail.com>
2024-03-25 23:56:44 +0000

    exercise 4.4

--- tests/blogList.test.js
+++ tests/blogList.test.js
@@ -1,7 +1,62 @@
+
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
-const { dummyList } = require('../utils/list_helpers')
+const dummyList = require('../utils/list_helpers').dummyList
+const totalLikes = require('../utils/list_helpers').totalLikes
 
-test('should return 1', () => {
-    assert.strictEqual(dummyList([]), 1)
-  }) 
+
+
+describe('should return total likes', () => {
+  const bloglist = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: "5a422ba71b54a676234d17fb",
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: "5a422bc61b54a676234d17fc",
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 2,
+      __v: 0
+    } 
+  ]
+
+  test('should return 1', () => {
+    assert.equal(dummyList([]), 1)
+  })
+
+  test('should return the single like for a single blogList', () => {
+    const result = totalLikes(bloglist)
+    assert.strictEqual(result, 29)
+  })
+
+})
--- utils/list_helpers.js
+++ utils/list_helpers.js
@@ -1,7 +1,15 @@
+
 const dummyList = (blogs) => {
   return 1
+};
+
+const totalLikes = (blogs) => {
+  const reducer = blogs.reduce((acc, curr) => acc + curr.likes, 0)
+  return blogs.length === 1 ? blogs[0].likes : reducer;
 }
+
 
 module.exports = {
   dummyList,
+  totalLikes
 }
